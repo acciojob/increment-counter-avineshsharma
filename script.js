e6a853d23d97d7270d5248bdc4019b55131b09f5
Avinesh sharma <142591660+avineshsharma@users.noreply.github.com>
2025-10-26 23:13:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let counter = 0;
+function increment() {
+	counter++;
+	document.getElementById("counter").textContent = counter;
+}
+
+document.getElementById("incrementBtn").addEventListener("click", increment);
